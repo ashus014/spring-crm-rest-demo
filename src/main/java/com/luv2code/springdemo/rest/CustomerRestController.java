a4b6877e2aaf43e2c26030351721692bf0c482af
Ashu Singh <ashu.ask.singh@gmail.com>
2021-11-28 14:39:12 +0530

    REST call to database to retrieve single record

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,14 @@ public class CustomerRestController {
 
         return customerService.getCustomers();
     }
+
+    //add mapping for GET /customers/{customerID}
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId){
+
+        Customer theCustomer = customerService.getCustomer(customerId);
+        return theCustomer;
+    }
+
 
 }
